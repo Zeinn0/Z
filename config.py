a445d638aa986984aabfb968449c674aeba3ae46
Shaher200 <139427103+Shaher200@users.noreply.github.com>
2023-08-15 21:21:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("28980480"))
+API_HASH = getenv("d851585d25de598ceaa6892c6253d6d8")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("6648614695:AAEy5UnrJl-tnr331811P7DBNpIT4QaeIaw", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("5134002906"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/8fa109bec9bb4dcf64e74.mp4")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/2514530559cc173845e3f.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/Photo-07-29-20")
+START_IMG = getenv("START_IMG", "https://graph.org/Photo-07-29-20")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SORS0C")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/UIU_II")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/hsbjndk")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHaHeRrrrrrr")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6300938349").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5134002906").split()))
 
 
 FAILED = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
